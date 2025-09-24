6b7a6d2291fbec4ffcea4a940efdac0def087eaf
Adam Mahmoudi <2396547@cmontmorency.qc.ca>
2025-09-23 21:15:29 -0400

    [ADD] : routes login et register etudiants

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -1,4 +1,5 @@
 import connectDB from "../util/database.js";
+import jwt from "jsonwebtoken";
 
 const db = await connectDB();
 
@@ -33,7 +34,7 @@ const getStudentByUsername = async (req, res, next) => {
       .from("etudiants")
       .select()
       .eq("username", req.params.name);
-    if (result.err) {
+    if (result.err != null) {
       throw err;
     }
     res.status(200).json(result.data);
@@ -45,10 +46,8 @@ const getStudentByUsername = async (req, res, next) => {
 const createStudent = async (req, res, next) => {
   try {
     const result = await db.from("etudiants").insert(req.body);
-    if (result.err) {
-      throw err;
-    }
-    res.status(201).json(result);
+
+    res.status(result.status).json(result);
   } catch (err) {
     next(err);
   }
@@ -77,10 +76,80 @@ const updateStudent = async (req, res, next) => {
   }
 };
 
+const loginStudent = async (req, res, next) => {
+  try {
+    const result = await db
+      .from("etudiants")
+      .select()
+      .eq("email", req.body.email)
+      .eq("password", req.body.password);
+    // if (result.err) {
+    //   throw err;
+    // }
+    if (result.error != null) {
+      res.status(result.status).json(result.error);
+      return next(new Error("Erreur de connexion"));
+    }
+    if (result.data.length <= 0) {
+      return next(new Error("Aucun utilisateur avec ces infos trouvé", 404));
+    }
+    let token;
+
+    token = jwt.sign(
+      { username: result.data[0].username, email: result.data[0].email },
+      "cleSuperSecrete",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      username: result.data[0].username,
+      email: result.data[0].email,
+      token: token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const registerStudent = async (req, res, next) => {
+  try {
+    const result = await db.from("etudiants").insert(req.body);
+    console.log(result);
+    if (result.error != null) {
+      res.status(result.status).json(result.error);
+      return next(new Error("Erreur de inscription"));
+    }
+    const student = await db
+      .from("etudiants")
+      .select()
+      .eq("email", req.body.email);
+
+    console.log(student);
+
+    let token;
+
+    token = jwt.sign(
+      { username: student.data[0].username, email: student.data[0].email },
+      "cleSuperSecrete",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      username: student.data[0].username,
+      email: student.data[0].email,
+      token: token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 export default {
   getAllStudents,
   getStudentById,
   getStudentByUsername,
   createStudent,
   updateStudent,
+  loginStudent,
+  registerStudent,
 };
